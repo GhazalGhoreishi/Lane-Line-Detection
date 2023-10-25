267b7ab1424af2e1ad6daa5ca09e285a820bbd03
GhazalGhoreishi <142857791+GhazalGhoreishi@users.noreply.github.com>
2023-10-25 11:00:15 +0330

    LineDec.py

--- LineDec.py
+++ LineDec.py
@@ -1,44 +1,171 @@
+import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import numpy as np
 
 class LineDetection:
 
-    def __init__(self, myframe):
-        self.frame = myframe  #initializig each frame to the object frame attribute
+    def __init__(self, binary_warped):
+        self.frame = binary_warped
+        self.out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
+        
 
-    def ld(self):
-        image = self.frame
-        image_copy = image.copy()
-        gray = cv2.cvtColor(image_copy, cv2.COLOR_BGR2GRAY)
+    def histogram(self):
+        binary_warped = self.frame
+        hist = np.sum(binary_warped[binary_warped.shape[0]//2:, :], axis=0)
+        return hist 
 
-        blur_gray = cv2.GaussianBlur(gray, (5, 5), 0)
-        edges = cv2.Canny(blur_gray, 10, 100)
+    def base(self):   # Returns leftx_base and rightx_base
+        histogram = self.histogram()
+        midpoint = int(histogram.shape[0]//2)
+        leftx_base = np.argmax(histogram[:midpoint])
+        rightx_base = np.argmax(histogram[midpoint:]) + midpoint 
+        return leftx_base, rightx_base
+    
+    def find_lane_pixels(self):
+        binary_warped = self.frame
+        leftx_base, rightx_base = self.base()
+        nwindows = 9
+        margin = 100
+        # Set minimum number of pixels found to recenter window
+        minpix = 50
+        # Set height of windows - based on nwindows above and image shape
+        window_height = int(binary_warped.shape[0]//nwindows)
+        # Identify the x and y positions of all nonzero pixels in the image
+        nonzero = binary_warped.nonzero()
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Current positions to be updated later for each window in nwindows
+        leftx_current = leftx_base
+        rightx_current = rightx_base
 
-        mask = np.zeros_like(edges)
-        ignore_mask_color = 255
+        # Create empty lists to receive left and right lane pixel indices
+        left_lane_inds = []
+        right_lane_inds = []
+        # Step through the windows one by one
+        for window in range(nwindows):
+            # Identify window boundaries in x and y (and right and left)
+            win_y_low = binary_warped.shape[0] - (window+1)*window_height
+            win_y_high = binary_warped.shape[0] - window*window_height
+            win_xleft_low = leftx_current - margin
+            win_xleft_high = leftx_current + margin
+            win_xright_low = rightx_current - margin
+            win_xright_high = rightx_current + margin
+            
+        # Identify the nonzero pixels in x and y within the window #
+            good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & 
+            (nonzerox >= win_xleft_low) &  (nonzerox < win_xleft_high)).nonzero()[0]
+            good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & 
+            (nonzerox >= win_xright_low) &  (nonzerox < win_xright_high)).nonzero()[0]
+            
+            # Append these indices to the lists
+            left_lane_inds.append(good_left_inds)
+            right_lane_inds.append(good_right_inds)
+            
+            # If you found > minpix pixels, recenter next window on their mean position
+            if len(good_left_inds) > minpix:
+                leftx_current = int(np.mean(nonzerox[good_left_inds]))
+            if len(good_right_inds) > minpix:        
+                rightx_current = int(np.mean(nonzerox[good_right_inds]))
+
+        # Concatenate the arrays of indices (previously was a list of lists of pixels)
+        try:
+            left_lane_inds = np.concatenate(left_lane_inds)
+            right_lane_inds = np.concatenate(right_lane_inds)
+        except ValueError:
+            # Avoids an error if the above is not implemented fully
+            pass
+
+    # Extract left and right line pixel positions
+        leftx = nonzerox[left_lane_inds]
+        lefty = nonzeroy[left_lane_inds] 
+        rightx = nonzerox[right_lane_inds]
+        righty = nonzeroy[right_lane_inds]
+
+        return leftx, lefty, rightx, righty
 
 
-        vertices = np.array([[(160, 650), (1100, 650), (750, 450), (560, 450)]], dtype=np.int32)
-        cv2.fillPoly(mask, vertices, ignore_mask_color)
-        masked_edges = cv2.bitwise_and(edges, mask)
+    def fit_polynomial(self):
+        binary_warped = self.frame
+        leftx, lefty, rightx, righty, out_img = self.find_lane_pixels()
 
-        rho = 1
-        theta = np.pi / 180
-        threshold = 1
-        min_line_lenght = 10
-        max_line_gap = 15
-        line_image = np.copy(image)*0
+        left_fit = np.polyfit(lefty, leftx, 2)
+        right_fit = np.polyfit(righty, rightx, 2)
 
-        lines = cv2.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]), min_line_lenght, max_line_gap)
+        ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+        try:
+            left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+            right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+        except TypeError:
+            left_fitx = 1*ploty**2 + 1*ploty
+            right_fitx = 1*ploty**2 + 1*ploty
+        
+        return left_fitx, right_fitx
+    
+    def fit_poly(self):
+        img_shape = self.frame.shape()
+        leftx, lefty, rightx, righty = self.find_lane_pixels()
+        left_fit = np.polyfit(lefty, leftx, 2)
+        right_fit = np.polyfit(righty, rightx, 2)
+        # Generate x and y values for plotting
+        ploty = np.linspace(0, img_shape[0]-1, img_shape[0])
+        ### TO-DO: Calc both polynomials using ploty, left_fit and right_fit ###
+        left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+        right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+        
+        return left_fitx, right_fitx, ploty
 
-        for line in lines:
-            for x1, y1, x2, y2 in line:
-                cv2.line(masked_edges, (x1, y1), (x2, y2), (255, 255, 255), 10)
+    def search_around_poly(self):
+        binary_warped = self.frame
+        margin = 100
+        nonzero = binary_warped.nonzero()
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        leftx, lefty, rightx, righty = self.find_lane_pixels()
+        left_fit = np.polyfit(lefty, leftx, 2)
+        right_fit = np.polyfit(righty, rightx, 2)
+        left_lane_inds = ((nonzerox > (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + 
+                        left_fit[2] - margin)) & (nonzerox < (left_fit[0]*(nonzeroy**2) + 
+                        left_fit[1]*nonzeroy + left_fit[2] + margin)))
+        right_lane_inds = ((nonzerox > (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + 
+                        right_fit[2] - margin)) & (nonzerox < (right_fit[0]*(nonzeroy**2) + 
+                        right_fit[1]*nonzeroy + right_fit[2] + margin)))
+        
+        # Again, extract left and right line pixel positions
+        leftx = nonzerox[left_lane_inds]
+        lefty = nonzeroy[left_lane_inds] 
+        rightx = nonzerox[right_lane_inds]
+        righty = nonzeroy[right_lane_inds]
 
-        color_edges = np.dstack((masked_edges, masked_edges, masked_edges))
+        # Fit new polynomials
+        left_fitx, right_fitx, ploty = self.fit_poly()
+        
+        ## Visualization ##
+        # Create an image to draw on and an image to show the selection window
+        out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
+        window_img = np.zeros_like(out_img)
+        # Color in left and right line pixels
+        out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+        out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
 
-        combo = cv2.addWeighted(color_edges, 0.8, image, 1, 0)
-        return masked_edges
+        # Generate a polygon to illustrate the search window area
+        # And recast the x and y points into usable format for cv2.fillPoly()
+        left_line_window1 = np.array([np.transpose(np.vstack([left_fitx-margin, ploty]))])
+        left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx+margin, 
+                                ploty])))])
+        left_line_pts = np.hstack((left_line_window1, left_line_window2))
+        right_line_window1 = np.array([np.transpose(np.vstack([right_fitx-margin, ploty]))])
+        right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx+margin, 
+                                ploty])))])
+        right_line_pts = np.hstack((right_line_window1, right_line_window2))
 
+        # Draw the lane onto the warped blank image
+        cv2.fillPoly(window_img, np.int_([left_line_pts]), (0,255, 0))
+        cv2.fillPoly(window_img, np.int_([right_line_pts]), (0,255, 0))
+        result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
+        
+        # Plot the polynomial lines onto the image
+        plt.plot(left_fitx, ploty, color='yellow')
+        plt.plot(right_fitx, ploty, color='yellow')
+        ## End visualization steps ##
+        
+        return result
