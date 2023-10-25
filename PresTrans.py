f2991ab1b48091da8968082d16db9d832b2c75ba
GhazalGhoreishi <142857791+GhazalGhoreishi@users.noreply.github.com>
2023-10-25 08:55:55 +0330

    PresTrans.py

--- PresTrans.py
+++ PresTrans.py
@@ -10,10 +10,10 @@ class PerspectiveTransformation:
     def pointingOut(self):
         image = self.frame
 
-        a = (160, 650)   # bottom left
-        b = (1100, 650)  # bottom right
-        c = (750, 450)   # top right
-        d = (560, 450)   # top left
+        a = (290, 650)   # bottom left
+        b = (1082, 650)  # bottom right
+        c = (722, 450)   # top right
+        d = (580, 450)   # top left
   
         cv2.circle(image, a, 5, (0, 0, 255), -1)
         cv2.circle(image, b, 5, (0, 0, 0), -1)
@@ -22,10 +22,10 @@ class PerspectiveTransformation:
 
         #Apply Geometical Transformation
         pts1 = np.float32([d, a, b, c])
-        pts2 = np.float32([(0, 0), (0, 720), (1280, 720), (1280, 0)])
+        pts2 = np.float32([(0, 0), (0, 200), (300, 200), (300, 0)])
         
         matrix = cv2.getPerspectiveTransform(pts1, pts2)
-        TransIMG = cv2.warpPerspective(image, matrix, (1280, 720))
+        TransIMG = cv2.warpPerspective(image, matrix, (300, 200))
 
 
         return TransIMG
